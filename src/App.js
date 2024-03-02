aee6f69290544076e3998784002d8a34b6ea1e5d
KEVIN ALEXSANDER DE JESUS <1050482123033@fatec.coorp>
2024-03-02 12:23:13 -0300

    finished the slide presentation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import contentArray from "./content"
 
 function App() {
+  const [ contentIndex, setContentIndex ] = useState(0)
+  const [ time, setTime ] = useState(0)
+  const [ start, setStart ] = useState(false)
+
+  useEffect(() => {
+    if ( start ) {
+      setTimeout(() => {
+        setTime(time + 1)
+        if (time >= 100 && contentIndex < contentArray.length - 1) {
+          setContentIndex(contentIndex + 1)
+          setTime(0)
+        }
+      }, 100)
+    }
+  }, [ time, start ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-full bg-gif-rocket bg-cover bg-center flex justify-center items-center text-purple-100 p-5 sm:p-10">
+      <div className="max-w-3xl p-5 sm:p-10 bg-purple-950/75 rounded-xl border border-purple-500 ">
+        {start ? (
+          <>
+            <h1 className="text-3xl sm:text-6xl mb-10 font-semibold">{contentArray[contentIndex].title}</h1>
+            <p className="text-lg sm:text-xl">{contentArray[contentIndex].content}</p>
+            <progress className="mt-10" id="file" value={time} max="100"></progress>
+          </>
+        ) : (
+          <button 
+            className="px-10 py-5 rounded-full bg-purple-800 text-lg font-semibold active:scale-95 transition-2"
+            onClick={() => { setStart(true) }}
+          >Começar Apresentação</button>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/content.js
@@ -0,0 +1,40 @@
+const content = [
+  {
+    "title": "Apresentação Pessoal",
+    "content": "Meu nome é Kevin e sou estagiário de desenvolvimento de software na Lenovo desde 2022."
+  },
+  {
+    "title": "Experiência na Lenovo",
+    "content": "Trabalho na Lenovo desde 2022, contribuindo para projetos de desenvolvimento de software."
+  },
+  {
+    "title": "Habilidades Técnicas",
+    "content": "Possuo habilidades em HTML, CSS, JavaScript, React, Next.js, Node.js, Java, Tailwind CSS, Bootstrap, MySQL, React Native, entre outros."
+  },
+  {
+    "title": "Desenvolvimento Front-end",
+    "content": "Especializado na criação de designs e sites responsivos para melhorar a experiência do usuário."
+  },
+  {
+    "title": "Frameworks CSS",
+    "content": "Trabalho com os frameworks Tailwind CSS e Bootstrap para facilitar o desenvolvimento e design de interfaces."
+  },
+  {
+    "title": "Banco de Dados",
+    "content": "Experiência em gerenciamento de bancos de dados utilizando MySQL."
+  },
+  {
+    "title": "Desenvolvimento Mobile",
+    "content": "Explorando o desenvolvimento mobile com React Native para ampliar minha gama de habilidades."
+  },
+  {
+    "title": "Vida de Estagiário",
+    "content": "A jornada como estagiário tem sido uma oportunidade de aprendizado constante e aplicação prática dos conhecimentos adquiridos."
+  },
+  {
+    "title": "Conclusão",
+    "content": "Estou entusiasmado com as oportunidades futuras e comprometido em contribuir positivamente para os projetos da equipe. Agradeço pela atenção!"
+  }
+]
+
+export default content
